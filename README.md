b7b953271d9f1c7a914911392da50d58128ef42c
PIETRO BLANDIZZI <pietroblandizzi@MacBook-Pro-di-PIETRO.local>
2020-11-17 12:10:41 +0100

    Add sudoku solver

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Sudoku_Solver-AI-Nanodegree
+
+First project of the Artificial Intelligence Nanodegree.
+It is implemented in python (python 3)
+It uses constraint programming with different strategies to solve sudoku
+Future work:
+Improve the algorithm and make it faster and able to solve even the hardest sudoku
---
+++ solution.py
@@ -0,0 +1,230 @@
+from utils import *
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+left_diagonal_units = list(a+b for a,b in zip(rows, cols))
+right_diagonal_units = [(rs + cs) for rs, cs in zip(rows[::-1], cols)]
+unitlist = row_units + column_units + square_units + [left_diagonal_units] + [right_diagonal_units]
+
+
+# Must be called after all units (including diagonals) are added to the unitlist
+units = extract_units(unitlist, boxes)
+peers = extract_peers(units, boxes)
+
+
+def naked_twins(values):
+    """Eliminate values using the naked twins strategy.
+
+    The naked twins strategy says that if you have two or more unallocated boxes
+    in a unit and there are only two digits that can go in those two boxes, then
+    those two digits can be eliminated from the possible assignments of all other
+    boxes in the same unit.
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict
+        The values dictionary with the naked twins eliminated from peers
+
+    Notes
+    -----
+    Your solution can either process all pairs of naked twins from the input once,
+    or it can continue processing pairs of naked twins until there are no such
+    pairs remaining -- the project assistant test suite will accept either
+    convention. However, it will not accept code that does not process all pairs
+    of naked twins from the original input. (For example, if you start processing
+    pairs of twins and eliminate another pair of twins before the second pair
+    is processed then your code will fail the PA test suite.)
+
+    The first convention is preferred for consistency with the other strategies,
+    and because it is simpler (since the reduce_puzzle function already calls this
+    strategy repeatedly).
+
+    See Also
+    --------
+    Pseudocode for this algorithm on github:
+    https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
+    """
+    # Get all boxes with 2 values
+    boxes_with_two_vals = [box for box in values.keys() if len(values[box]) == 2]
+    # Get peers of each box with same elements
+    peers_of_each_box = [[box, peer] for box in boxes_with_two_vals for peer in peers[box] if values[box] == values[peer]]
+
+    # For each of those boxes get peers with the same values
+    for box, peer in peers_of_each_box:
+        common_peers = set(peers[box]).intersection(set(peers[peer]))
+
+        for common_peer in common_peers:
+            for digit in values[box]:
+                values = assign_value(values, common_peer, values[common_peer].replace(digit, ''))
+
+    return values
+
+
+def eliminate(values):
+    """Apply the eliminate strategy to a Sudoku puzzle
+
+    The eliminate strategy says that if a box has a value assigned, then none
+    of the peers of that box can have the same value.
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict
+        The values dictionary with the assigned values eliminated from peers
+    """
+    solved_elements = [box for box in values.keys() if len(values[box]) == 1]
+
+    for box in solved_elements:
+        # Get all peers
+        peers_of_box = peers[box]
+
+        for peer in peers_of_box:
+            values[peer] = values[peer].replace(values[box], '')
+    return values
+
+
+def only_choice(values):
+    """Apply the only choice strategy to a Sudoku puzzle
+
+    The only choice strategy says that if only one box in a unit allows a certain
+    digit, then that box must be assigned that digit.
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict
+        The values dictionary with all single-valued boxes assigned
+
+    Notes
+    -----
+    You should be able to complete this function by copying your code from the classroom
+    """
+    for unit in unitlist:
+        for d in '123456789':
+            list_box_with_d = [box for box in unit if d in values[box]]
+            if len(list_box_with_d) == 1:
+                values[list_box_with_d[0]] = d
+
+    return values
+
+
+def reduce_puzzle(values):
+    """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict or False
+        The values dictionary after continued application of the constraint strategies
+        no longer produces any changes, or False if the puzzle is unsolvable 
+    """
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+
+def search(values):
+    """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
+    that cannot be solved by repeated reduction alone.
+
+    Parameters
+    ----------
+    values(dict)
+        a dictionary of the form {'box_name': '123456789', ...}
+
+    Returns
+    -------
+    dict or False
+        The values dictionary with all boxes assigned or False
+
+    Notes
+    -----
+    You should be able to complete this function by copying your code from the classroom
+    and extending it to call the naked twins strategy.
+    """
+    values = reduce_puzzle(values)
+    if not values:
+        return False
+    # Choose one of the unfilled squares with the fewest possibilities
+    if all(len(values[s]) == 1 for s in boxes):
+        return values
+
+    n, selected_min_box = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+
+    for value in values[selected_min_box]:
+        new_sudoku = values.copy()
+        new_sudoku[selected_min_box] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+def solve(grid):
+    """Find the solution to a Sudoku puzzle using search and constraint propagation
+
+    Parameters
+    ----------
+    grid(string)
+        a string representing a sudoku grid.
+        
+        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+
+    Returns
+    -------
+    dict or False
+        The dictionary representation of the final sudoku grid or False if no solution exists.
+    """
+    values = grid2values(grid)
+    values = search(values)
+    return values
+
+
+if __name__ == "__main__":
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(grid2values(diag_sudoku_grid))
+    result = solve(diag_sudoku_grid)
+    display(result)
+
+    try:
+        import PySudoku
+
+        PySudoku.play(grid2values(diag_sudoku_grid), result, history)
+
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
